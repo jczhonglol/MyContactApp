608aa78539cfd8264e02479564cb105fab577e76
jczhonglol <ZhongJ9351@sduhsd.lan>
2017-05-15 09:30:42 -0700

    Created a method addData to add data when the data button is pressed. Then added two more fields Age and Address, two columns, toast message
    toast messages will indicate whether data was added successfully or not

--- MyContactApp/app/src/main/java/com/example/zhongj9351/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/zhongj9351/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.zhongj9351.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,7 +13,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2 = "MAIN";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "ADDRESS";
+    public static final String COL_4 = "AGE";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -27,5 +30,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData (String name, String address, String age){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, address);
+        contentValues.put(COL_4, age);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
+
+
     }
 }
--- MyContactApp/app/src/main/java/com/example/zhongj9351/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhongj9351/mycontactapp/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.zhongj9351.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAddress;
+    EditText editAge;
     Button btnAddData;
 
     @Override
@@ -17,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper (this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editName = (EditText) findViewById(R.id.editText_address);
+        editName = (EditText) findViewById(R.id.editText_age);
+
     }
+
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editAge.getText().toString());
+        int duration = Toast.LENGTH_SHORT;
+        Toast toastSuccessful = Toast.makeText(this, "Data insertion successful", duration);
+        Toast toastUnsuccessful = Toast.makeText(this, "Data insertion unsuccessful", duration);
+        if(isInserted == true){
+            Log.d("My Contact", "Data  insertion successful");
+            //create toast message to user indicating data inserted correctly
+            toastSuccessful.show();
+        }
+        else{
+            Log.d("My Contact", "Data  insertion NOT successful");
+            //create toast message to user indicating data inserted incorrectly
+            toastUnsuccessful.show();
+        }
+    }
+
+
 }
